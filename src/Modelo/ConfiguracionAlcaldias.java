9c284a435bf8e245dde6f0d36d0a265935a0ea4f
Alexia Pimentel 3 <115095487+AlePime31@users.noreply.github.com>
2024-11-09 10:15:55 -0600

    Update ConfiguracionAlcaldias.java
    Se agregaron mas alcaldias

--- src/Modelo/ConfiguracionAlcaldias.java
+++ src/Modelo/ConfiguracionAlcaldias.java
@@ -10,7 +10,12 @@ public class ConfiguracionAlcaldias {
         coordenadasAlcaldias.put("Tlalpan", new Coordenadas(19.4326, -99.1332));
         coordenadasAlcaldias.put("Coyoacan", new Coordenadas(19.4065, -99.1625));
         coordenadasAlcaldias.put("Iztapalapa", new Coordenadas(19.842, -99.2123));
-        // Agrega más alcaldías con sus respectivas coordenadas predeterminadas
+        coordenadasAlcaldias.put("Miguel Hidalgo", new Coordenadas(19.4332, -99.1911));
+        coordenadasAlcaldias.put("Venustiano Carranza", new Coordenadas(19.4215, -99.1416));
+        coordenadasAlcaldias.put("Azcapotzalco", new Coordenadas(19.5000, -99.2000));
+        coordenadasAlcaldias.put("Xochimilco", new Coordenadas(19.2500, -99.1000));
+        coordenadasAlcaldias.put("Gustavo A. Madero", new Coordenadas(19.5075, -99.1381));
+        coordenadasAlcaldias.put("Benito Juárez", new Coordenadas(19.4000, -99.1700));
     }
 
     public static Coordenadas obtenerCoordenadas(String alcaldia) {
